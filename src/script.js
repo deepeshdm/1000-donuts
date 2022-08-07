36acc7d5eb0333095345bde4127abc2917ca2842
Deepesh <deepeshmhatre133@gmail.com>
2022-08-07 17:06:45 +0530

    added audio

--- src/script.js
+++ src/script.js
@@ -140,6 +140,26 @@ fontLoader.load('./fonts/helvetiker_regular.typeface.json', function (font) {
 
 //---------------------------------------------------------
 
+// Add Sound 
+
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( '/sounds/ambience.ogg', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
+
+//---------------------------------------------------------
+
 // Animation loop 
 
 function animate() {
